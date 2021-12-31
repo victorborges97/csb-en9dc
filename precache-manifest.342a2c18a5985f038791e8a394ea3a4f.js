dceaf3636895764f649299d9cdcffb70709d0510
Code Sandbox <hello@codesandbox.io>
2021-12-31 20:46:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.342a2c18a5985f038791e8a394ea3a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7ce507e3cfe17eacd8dfdb10d1ee31",
+    "url": "https://victorborges97.github.io/csb-en9dc/index.html"
+  },
+  {
+    "revision": "1d8cd2d3acd029dc35a9",
+    "url": "https://victorborges97.github.io/csb-en9dc/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "e965f67d99f9c6582ad6",
+    "url": "https://victorborges97.github.io/csb-en9dc/static/css/main.60036c3e.chunk.css"
+  },
+  {
+    "revision": "1d8cd2d3acd029dc35a9",
+    "url": "https://victorborges97.github.io/csb-en9dc/static/js/2.044b1235.chunk.js"
+  },
+  {
+    "revision": "e965f67d99f9c6582ad6",
+    "url": "https://victorborges97.github.io/csb-en9dc/static/js/main.04f172d2.chunk.js"
+  },
+  {
+    "revision": "b899f4e28e9195ce36ce",
+    "url": "https://victorborges97.github.io/csb-en9dc/static/js/runtime~main.4ec5927d.js"
+  }
+]);
